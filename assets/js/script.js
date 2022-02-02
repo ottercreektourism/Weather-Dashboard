16b26cdfa686187af1816a3cb71c292fc4ad53b8
Marissa Flynn <mkflynn13@gmail.com>
2022-02-02 12:57:23 -0500

    Added coments to html and js, and a possible function for displaying the 5 day forecast.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+// API information
 const apiKey = "1d0e728c8ec9d18a5f99aaa0096cbec3";
-var enterCity = $('enterCity');
+var geocoder = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}"
+
+
+// hard code the city for testing. will change later
 var enterCity = "Boston"
+// var enterCity = $('enterCity');
+
 var searchHistoryList = $('searchHistoryList');
 // var selectedCity = $('enterCity');
 var userCity = $('userCity');
@@ -25,7 +31,7 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&u
     var weather = data.weather[0].main;
     var windSpeed = Math.floor(data.wind.speed);
     var humidity = data.main.humidity;
-    // var UVIndex = data.
+    // var UVIndex = data. //use the lat and lon to find the UVIndex
 
     $('.icon').attr('src', icon);
     $('.cityWeather').append(weather);
@@ -36,7 +42,7 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&u
 });
 
 // Function to add event listener to click button
-
+// trying to see if the keys work to troubleshoot for the .onclick function not working
 enterCity.addeventlistener('keyup', (e)=>{
     const searchString = e.target.value.toLowerCase();
     const filteredResults = allCities.filter((city)=>{
@@ -51,7 +57,7 @@ enterCity.addeventlistener('keyup', (e)=>{
 $("enterCity").keyup(function(event){
     console.log(event.target.value);
     event.preventDefault();
-    alert("The paragraph was clicked.");
+    alert("The paragraph was clicked."); // example from another source, trying to make it work
     var enterCity = $('enterCity').val();
     console.log(enterCity);
 
@@ -71,6 +77,7 @@ const fetchLocation = () => {
     }
 }
 
+// searchLocation function is for calling the city entered from the API, and handling if the entered city is invalid
 const searchLocation = (event) =>{
     event.preventDefault();
     var enterCity = $("#enterCity").val;
@@ -85,3 +92,43 @@ const searchLocation = (event) =>{
     enterCity.val("");
 }
 $('#searchBtn').on('submit', searchLocation);
+
+
+// function displayWeather(data) {
+//     for (var i = 1; i < 6; i++) {
+//       const card = $("<div>");
+//       card.addClass("card");
+//       card.attr("style", "width: 18rem");
+  
+//       const cardBody = $("<div>");
+//       cardBody.addClass("card-body");
+  
+//       const date = $("<h5>");
+//       date.addClass("card-title");
+//       date.text(moment(data.daily[i].dt, "X").format("ddd, MMM Do"));
+  
+//       const icon = $("<img>");
+//       icon.addClass("card-img-top");
+//       icon.attr("src",); // http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png
+      
+    
+//       icon.attr("alt", data.daily[i].weather[0].description);
+  
+//       cardBody.append(date, icon);
+  
+//       const list = $("<ul>");
+//       list.addClass("list-group list-group-flush");
+  
+//       const temp = $(<li>Temp: ${data.daily[i].temp.day} °F</li>);
+//       const wind = $(<li>Wind: ${data.daily[i].wind_speed} mph</li>);
+//       const humidity = $(<li>Humidity: ${data.daily[i].humidity} %</li>);
+//       temp.addClass("list-group-item");
+//       wind.addClass("list-group-item");
+//       humidity.addClass("list-group-item");
+  
+//       list.append(temp, wind, humidity);
+  
+//       card.append(cardBody, list);
+//       $(".weather-cards").append(card);
+//     }
+//   }
