e300af4f232fc35e55f4887f44337bc8caaad6ca
Marissa Flynn <mkflynn13@gmail.com>
2022-02-04 14:37:46 -0500

    Completely reorganizing the whole thing to learn how to complete the project. Added CSS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,28 @@
 // API information
 const apiKey = "1d0e728c8ec9d18a5f99aaa0096cbec3";
 var geocoder = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}"
-
-
-// hard code the city for testing. will change later
-var enterCity = "Boston"
 // var enterCity = $('enterCity');
+// hard code the city for testing. will change later
+// var searchBtn = $('searchBtn');
+var enterCity = "Boston"
+var cityName = $('.cityName');
+// var enterCity = $('.cityName')
+var cityDate = $('.cityDate');
+var icon = $('.icon');
+var cityWeather = $('.cityWeather');
+var cityTemp = $('.cityTemp');
+var cityWind = $('.cityWind');
+var cityHumidity = $('.cityHumidity');
+var cityUVIndex = $('.cityUVIndex');
+var fiveDayForecast = $('fiveDayForecast');
+var dateTrackerElement = $('#currentDay');
 
-var searchHistoryList = $('searchHistoryList');
-// var selectedCity = $('enterCity');
-var userCity = $('userCity');
-var searchBtn = $('searchBtn');
+// Function to make the search button search for a city to call from the API
 
-var userCityCurrent = ""
-var userCityArray = []
-
-function getWeather(event){
-    event.preventDefault();
-    // var selectedCity = $('enterCity');
-}
-
-// let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&units=imperial&APPID=" + apiKey;
 
 
 // Function to retrieve data from the API and display it to the page. Successful method.
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&units=imperial&APPID=" + apiKey, function(data){
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&units=imperial&APPID=" + apiKey, function (data) {
     // console.log(data);
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var temp = Math.floor(data.main.temp);
@@ -33,32 +31,78 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&u
     var humidity = data.main.humidity;
     // var UVIndex = data. //use the lat and lon to find the UVIndex
 
+    $('.cityName').append(enterCity);
     $('.icon').attr('src', icon);
     $('.cityWeather').append(weather);
     $('.cityTemp').append("Temperature: " + temp + " °F");
     $('.cityWind').append("Wind Speed: " + windSpeed + " MPH");
     $('.cityHumidity').append("Humidity: " + humidity + "%");
+    
 
 });
 
+// using moment to track the current date and time
+function timeTracker() {
+    var currentDay = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+    dateTrackerElement.text(currentDay);
+}
+// Setting timer interval
+setInterval(timeTracker, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userCityCurrent = ""
+// var userCityArray = []
+
+// function getWeather(event) {
+//     event.preventDefault();
+    // var selectedCity = $('enterCity');
+
+
+// let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&units=imperial&APPID=" + apiKey;
+
+
+
+
 
 // Trying to figure out how to call the function using fetch instead of the $.getJSON method since we're learning about fetch in class
-function oneCallURL(lat, lon){
-var oneCallURL = "https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&units=imperial&APPID=" + apiKey;
-fetch(oneCallURL)
-    .then(function (res){
-        return res.json()
-    })
-    .then(function (data){
-        temp.textContent = data.current.temp
-    })
+function oneCallURL(lat, lon) {
+    var oneCallURL = "https://api.openweathermap.org/data/2.5/weather?q=" + enterCity + "&units=imperial&APPID=" + apiKey;
+    fetch(oneCallURL)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            temp.textContent = data.current.temp
+        })
 }
 
 // Function to add event listener to click button
 // trying to see if the keys work to troubleshoot for the .onclick function not working. will change to click event listener.
-enterCity.addeventlistener('keyup', (event)=>{
+enterCity.addeventlistener('keyup', (event) => {
     const searchString = event.target.value.toLowerCase();
-    const filteredResults = allCities.filter((city)=>{
+    const filteredResults = allCities.filter((city) => {
         city.name.toLowerCase().includes(searchString);
 
     })
@@ -67,16 +111,41 @@ enterCity.addeventlistener('keyup', (event)=>{
 
 // Function to display current conditions for selected City
 // trying new method here, same function as above.
-$("enterCity").keyup(function(event){
+
+let allCities = [
+    {
+        name: 'Charlotte'
+    },
+    {
+        name: 'Rockford'
+    },
+    {
+        name: 'New York'
+    }
+]
+$("#enterCity").keyup(function (event) {
+
+    const searchString = event.target.value.toLowerCase();
+    const filteredResults = allCities.filter((city) => {
+        city.name.toLowerCase().includes(searchString);
+
+    })
     console.log(event.target.value);
     event.preventDefault();
-    var enterCity = $('enterCity').val();
+    var enterCity = $('#enterCity').val();
     console.log(enterCity);
 
     // var city = enterCity.val;
     // console.log(city);
-    
+
 });
+
+
+
+
+
+
+
 // var enterCity = enterCity.val();
 // console.log(enterCity);
 
@@ -90,12 +159,12 @@ $("enterCity").keyup(function(event){
 // }
 
 // searchLocation function is for calling the city entered from the API, and handling if the entered city is invalid
-const searchLocation = (event) =>{
+const searchLocation = (event) => {
     event.preventDefault();
     var enterCity = $("#enterCity").val;
     enterCity = enterCity.trim();
 
-    if(!enterCity) {
+    if (!enterCity) {
         console.error("Please enter a location!");
         return;
     }
@@ -112,35 +181,35 @@ $('#searchBtn').on('submit', searchLocation);
 //       const card = $("<div>");
 //       card.addClass("card");
 //       card.attr("style", "width: 18rem");
-  
+
 //       const cardBody = $("<div>");
 //       cardBody.addClass("card-body");
-  
+
 //       const date = $("<h5>");
 //       date.addClass("card-title");
 //       date.text(moment(data.daily[i].dt, "X").format("ddd, MMM Do"));
-  
+
 //       const icon = $("<img>");
 //       icon.addClass("card-img-top");
 //       icon.attr("src",); // http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png
-      
-    
+
+
 //       icon.attr("alt", data.daily[i].weather[0].description);
-  
+
 //       cardBody.append(date, icon);
-  
+
 //       const list = $("<ul>");
 //       list.addClass("list-group list-group-flush");
-  
+
 //       const temp = $(<li>Temp: ${data.daily[i].temp.day} °F</li>);
 //       const wind = $(<li>Wind: ${data.daily[i].wind_speed} mph</li>);
 //       const humidity = $(<li>Humidity: ${data.daily[i].humidity} %</li>);
 //       temp.addClass("list-group-item");
 //       wind.addClass("list-group-item");
 //       humidity.addClass("list-group-item");
-  
+
 //       list.append(temp, wind, humidity);
-  
+
 //       card.append(cardBody, list);
 //       $(".weather-cards").append(card);
 //     }
